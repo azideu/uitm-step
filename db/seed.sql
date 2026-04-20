3f9f0153945d624d52850060b6516bd08b48550c
azideu <addinzidane@gmail.com>
2026-04-20 20:52:12 +0800

    chore: update student seed data with actual user records

--- db/seed.sql
+++ db/seed.sql
@@ -18,11 +18,11 @@ INSERT INTO users (student_id, name, email, password, campus, role) VALUES
 
 -- Insert Students (Password is 'password')
 INSERT INTO users (student_id, name, email, password, campus, role) VALUES
-('2021111111', 'Ahmad Ali', '2021111111@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Shah Alam', 'student'),
-('2021222222', 'Siti Nurhaliza', '2021222222@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Puncak Alam', 'student'),
-('2021333333', 'Muthu Kumar', '2021333333@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Jengka', 'student'),
-('2021444444', 'Wong Mei Lin', '2021444444@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Shah Alam', 'student'),
-('2021555555', 'Farid Kamil', '2021555555@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Seri Iskandar', 'student');
+('2024663124', 'Ahmad Zaidan', '2024663124@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Shah Alam', 'student'),
+('2024237928', 'Amanda Saffieya', '2024237928@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Puncak Alam', 'student'),
+('2024284386', 'Ainun Nadiah', '2024284386@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Jengka', 'student'),
+('2024288856', 'Adam Khairi', '2024288856@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Shah Alam', 'student'),
+('2024285674', 'Muhammad Ridhuan', '2024285674@student.uitm.edu.my', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Seri Iskandar', 'student');
 
 -- Insert Tags
 INSERT INTO tags (name) VALUES ('Programming'), ('Design'), ('Writing'), ('Tutor'), ('Video');
